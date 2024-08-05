2d8e16d040e070d55d8c4d6229a322dc85b8169a
shivanya-chandra <69587802+shivanya-chandra@users.noreply.github.com>
2024-08-05 08:22:49 +0530

    css should work

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const expressLayouts = require("express-ejs-layouts");
 const partials = require('express-partials');
 const chat = require("./models/chatModel");
 
-
 const app = express();
-
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
@@ -19,21 +16,12 @@ const io = new Server(server);
 app.set("view engine", "ejs");
 
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "/public")));
-app.use(express.static('public', {
-  setHeaders: (res, path, stat) => {
-    if (path.endsWith('.ttf')) {
-      res.set('Content-Type', 'font/ttf');
-    }
-  }
-}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(expressLayouts);
 app.use(express.json());
 app.set("views", path.join(__dirname, "views"));
-// app.use(partials());
 app.use('/socket.io', express.static(path.join(__dirname, 'node_modules', 'socket.io', 'client-dist')));
-
 
 // Socket.IO setup
 io.on('connection', (socket) => {
@@ -65,7 +53,6 @@ io.on('connection', (socket) => {
     });
     console.log(newMessage);
 
-
     newMessage.save().then(() => {
       console.log('message saved');
     }).catch(err => {
@@ -82,25 +69,22 @@ io.on('connection', (socket) => {
 const indexRouter = require("./routes/indexRouter");
 const authRouter = require("./routes/authRouter");
 const dashboardRouter = require("./routes/dashboardRouter");
-menteeRouter = require("./routes/menteeRouter");
-mentorRouter = require("./routes/mentorRouter");
+const menteeRouter = require("./routes/menteeRouter");
+const mentorRouter = require("./routes/mentorRouter");
 const profileRouter = require("./routes/profileRouter");
 const requestRouter = require("./routes/requestRouter");
 const helloRouter = require("./routes/helloRouter");
 const friendRouter = require("./routes/friendRouter");
 const chatRouter = require("./routes/chatRouter");
-const profileViewRouter = require("./routes/profileViewRouter")
+const profileViewRouter = require("./routes/profileViewRouter");
 const mentorViewRouter = require("./routes/mentorViewRouter");
-
 
 app.use(indexRouter);
 app.use("/auth", authRouter);
 app.use("/dashboard", dashboardRouter);
 app.use("/mentee", menteeRouter);
 app.use("/mentor", mentorRouter);
-
-
-app.use(requestRouter)
+app.use(requestRouter);
 app.use(profileRouter);
 app.use(friendRouter);
 app.use(chatRouter);
@@ -108,7 +92,7 @@ app.use(profileViewRouter);
 app.use(mentorViewRouter);
 
 const PORT = process.env.PORT || 8080;
-const MONGO_URI = 'mongodb+srv://aayanagarwal05:hello@cluster0.ipsboy4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI, { useUnifiedTopology: true }).then(() => {
   console.log("connected to MongoDB");
@@ -118,6 +102,3 @@ mongoose.connect(MONGO_URI, { useUnifiedTopology: true }).then(() => {
 }).catch((err) => {
   console.log(err);
 });
-
-
-
