3f7b6069b9647b663cf4285fcec0c26de9da09b8
shivanya-chandra <69587802+shivanya-chandra@users.noreply.github.com>
2024-08-05 13:59:25 +0530

    updste

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const chat = require("./models/chatModel");
 const cors = require("cors");
 
 const app = express();
-const http = require('http');
+const http = require('https');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
